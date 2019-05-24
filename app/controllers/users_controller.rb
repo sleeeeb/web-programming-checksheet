6c42dc5be78024ec9644fe946033fc15b08c93af
haruki <nagasoe0326@gmail.com>
2019-05-24 10:01:08 +0900

    mark up mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
 class UsersController < ApplicationController
+
   def show
+  @user=User.find(1)
+  @trophys=[]
+  case current_user.comments.length
+  when 100...20000
+    @trophys << Trophy.find(8)
+    @trophys << Trophy.find(9)
+    @trophys << Trophy.find(10)
+    @trophys << Trophy.find(11)
+  when  50...100
+    @trophys << Trophy.find(8)
+    @trophys << Trophy.find(9)
+    @trophys << Trophy.find(10)
+  when 10...50
+    @trophys << Trophy.find(8)
+    @trophys << Trophy.find(9)
+  when 1...10
+    @trophys << Trophy.find(8)  
   end
+
+  case current_user.likes.length
+  when 50...20000
+    @trophys << Trophy.find(12)
+    @trophys << Trophy.find(13)
+  when 10...50
+    @trophys << Trophy.find(12)  
+  end
+
+end
 end
