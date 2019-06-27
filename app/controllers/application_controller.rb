4d4f948aa5fc58d0d27e9b7e936a52e1df66ca3b
haruki <nagasoe0326@gmail.com>
2019-06-27 09:19:57 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :get_information 
+  before_action :get_information
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username,:start_date])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :start_date])
   end
+
   def get_information
-    @genres=Genre.all
-    todo_contents=[]
+    @genres = Genre.all.includes([:contents, :user_checks])
+    todo_contents = []
     if current_user.present?
-    user_check=User.find(current_user.id).user_checks.each do |user_check| 
-      checks=Check.where(user_check_id:user_check.id).where(["flag > ? ", 5])
-      checks.each do |check|
-        todo_contents<< Content.find(check.content_id)
-      end
+      @user_checks = current_user.user_checks
     end
-      @todo_contents=todo_contents
-
-    end
+    @todo_contents = todo_contents
   end
-
 end
