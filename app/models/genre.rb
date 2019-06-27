e1e44f26a6b372eac749b57c731bb8d72b927f47
haruki <nagasoe0326@gmail.com>
2019-06-27 09:22:06 +0900

    fix for new column

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,4 @@
 class Genre < ApplicationRecord
   has_many :contents
-
+  has_many :user_checks
 end
--- app/models/user_check.rb
+++ app/models/user_check.rb
@@ -2,4 +2,5 @@ class UserCheck < ApplicationRecord
   has_many :checks
   accepts_nested_attributes_for :checks
   belongs_to :user
+  belongs_to :genre
 end
