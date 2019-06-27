613b6b1309f308215a485a054f6e4c9741de5032
haruki <nagasoe0326@gmail.com>
2019-06-27 09:21:15 +0900

    auto format and remove unuse column

--- app/controllers/user_checks_controller.rb
+++ app/controllers/user_checks_controller.rb
@@ -1,60 +1,57 @@
 class UserChecksController < ApplicationController
-require 'byebug'
+  require 'byebug'
+
   def index
-    @genres=Genre.all
-    @user_checks=current_user.user_checks
-    texts=[]
-    user_check=User.find(current_user.id).user_checks.each do |user_check| 
-      checks=Check.where(user_check_id:user_check.id).where(["flag > ? ", 5])
+    @genres = Genre.all
+    @user_checks = current_user.user_checks
+    texts = []
+    user_check = User.find(current_user.id).user_checks.each do |user_check|
+      checks = Check.where(user_check_id: user_check.id).where(['flag > ? ', 5])
       checks.each do |check|
-        texts<< Content.find(check.content_id).text
+        texts << Content.find(check.content_id).text
       end
-      @texts=texts
+      @texts = texts
     end
   end
+
   def show
-    @user_check = UserCheck.find_by(user_id:current_user.id)
-    @checks=@user_check.checks.first
-    end
-
-
+    @user_check = UserCheck.find_by(user_id: current_user.id)
+    @checks = @user_check.checks.first
+  end
 
   def new
-      @user_check = UserCheck.new
-      @user_checks = Content.last.id.times do 
-        @user_check.checks.build
-      end
+    @user_check = UserCheck.new
+    @user_checks = Content.last.id.times do
+      @user_check.checks.build
+    end
   end
+
   def edit
-    @user_check = UserCheck.find_by(user_id:current_user.id)
-    @checks=@user_check.checks.firstend
+    @user_check = UserCheck.find_by(user_id: current_user.id)
+    @checks = @user_check.checks.firstend
   end
 
   def create
     @user_check = current_user.user_checks.create(user_check_params)
-    @user_check.score = @user_check.checks.where(["level > ? ", 2]).length 
+    @user_check.score = @user_check.checks.where(['level > ? ', 2]).length
     @user_check.save
     current_user.update(rank: (current_user.rank.to_i + @user_check.score))
     redirect_to root_path
-
   end
+
   def update
     @user_check = current_user.user_checks.find_by(id: params[:id])
     @user_check.update(user_check_params)
-    @user_check.score = @user_check.checks.where(["level > ? ", 2]).length 
-    @user_check.saveexit
-
+    @user_check.score = @user_check.checks.where(['level > ? ', 2]).length
+    @user_check.save
     redirect_to root_path
-    
   end
-
 
   def user_check_params
     params.require(:user_check).permit(
-    :genre_id,
-    :user_id,
-    checks_attributes: [:id,:level,:flag,:content_id]
+      :genre_id,
+      :user_id,
+      checks_attributes: [:id, :level, :flag, :content_id],
     )
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,40 @@
 class UsersController < ApplicationController
-
   def enter
-        render :layout => nil
+    render :layout => nil
   end
 
   def about
   end
-  
+
   def index
-  @genres=Genre.all
   end
 
   def show
-    @user=User.find(params[:id])
-    @trophys=[]
+    @user = User.find(params[:id])
+    @trophys = []
     case current_user.comments.length
-      when 100...20000
-        @trophys << Trophy.find(8)
-        @trophys << Trophy.find(9)
-        @trophys << Trophy.find(10)
-        @trophys << Trophy.find(11)
-      when  50...100
-        @trophys << Trophy.find(8)
-        @trophys << Trophy.find(9)
-        @trophys << Trophy.find(10)
-      when 10...50
-        @trophys << Trophy.find(8)
-        @trophys << Trophy.find(9)
-      when 1...10
-        @trophys << Trophy.find(8)  
+    when 100...20000
+      @trophys << Trophy.find(8)
+      @trophys << Trophy.find(9)
+      @trophys << Trophy.find(10)
+      @trophys << Trophy.find(11)
+    when 50...100
+      @trophys << Trophy.find(8)
+      @trophys << Trophy.find(9)
+      @trophys << Trophy.find(10)
+    when 10...50
+      @trophys << Trophy.find(8)
+      @trophys << Trophy.find(9)
+    when 1...10
+      @trophys << Trophy.find(8)
     end
 
     case current_user.likes.length
-      when 50...20000
-        @trophys << Trophy.find(12)
-        @trophys << Trophy.find(13)
-      when 10...50
-        @trophys << Trophy.find(12)  
+    when 50...20000
+      @trophys << Trophy.find(12)
+      @trophys << Trophy.find(13)
+    when 10...50
+      @trophys << Trophy.find(12)
+    end
   end
-
-end
 end
