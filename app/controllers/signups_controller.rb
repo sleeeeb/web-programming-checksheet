441f14f5e0939c4226373f2c86563637dff209c2
haruki <nagasoe0326@gmail.com>
2019-07-04 20:27:15 +0900

    Create signups_controller.rb

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,74 @@
+class SignupsController < ApplicationController
+  def sms_confirmation_send
+    @profile=Profile.new
+    flash.delete(:notice)
+  end
+
+  def sms_confirmation_certify
+    @profile=current_user.profile.assign_attributes(profile_params)
+    if  current_user.profile.valid?(:sms_confirmation_send)
+      current_user.profile.update(profile_params)
+    else
+      @profile=Profile.new
+      @errors='未記入箇所があります'
+      render 'sms_confirmation_send'
+    end
+  end
+
+
+  def oauth
+    @user = User.new
+
+if session['devise.google_data']
+  @user.email = session['devise.google_data']['info']['unverified_email']
+end
+# byebug
+if session['devise.facebook_data']
+  @user.email = session['devise.facebook_data']['info']['email']
+end
+  end
+
+  def oauth_facebook
+    @user = User.new
+    @user.email = session["devise.facebook_data"]['info']['email']
+  end
+
+  def address_create
+    @profile=current_user.profile.assign_attributes(profile_params)
+    if current_user.profile.valid?(:hoge)
+      current_user.profile.update(profile_params)
+    else
+      @profile=current_user.profile
+      @errors='未記入箇所があります'
+      render 'address'
+    end
+    case params[:move_from]
+      when 'signup'
+        redirect_to new_user_card_path(current_user.id)
+      when 'purchase'
+        redirect_to new_item_purchase_path(params[:item_id])
+    end
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def succesful
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:phone_number,:family_name, :last_name, :kana_family_name, :kana_last_name, :postalcode, :address_prefecture, :address_city, :address_street_number,:address_building_name)
+  end
+
+  def use_check_address_nil?
+    false
+  end
+end
