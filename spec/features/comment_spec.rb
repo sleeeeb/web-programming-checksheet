711e8f8afd0c33e16b035bdd4d37ca03d1a26983
haruki <nagasoe0326@gmail.com>
2019-07-15 13:16:15 +0900

    update comment_rspec

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -11,19 +11,13 @@ feature 'comment', type: :feature do
     fill_in 'user_password', with: user.password
     first('input[name="commit"]').click
   end
-  scenario 'post_and_edit_and_destroy' do
+  scenario 'post_and_destroy' do
     # コメントをする
     expect do
       visit content_path(Content.first)
       fill_in 'comment_text', with: 'test_comment'
       first('input[name="commit"]').click
     end.to change(Comment, :count).by(1)
-    # コメントを編集する
-    visit content_path(Content.first)
-    first('a[class="edit_commment_button"]').click
-    fill_in 'comment_text', with: 'fixed_comment'
-    first('input[name="commit"]').click
-    expect(page).to have_content('fixed_comment')
     # コメントを削除する
     expect do
       visit content_path(Content.first)
